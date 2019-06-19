44f2be1f39c74f601405ab05aa18bae91c00c728
cjy769969037 <769969037@qq.com>
2019-06-19 15:33:18 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
-function component(){
+function component() {
+
     var element = document.createElement('div');
+    var btn = document.createElement('button');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
     
-    element.innerHTML = _.join(['Hello','webpack']);
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
     return element;
 }
 
